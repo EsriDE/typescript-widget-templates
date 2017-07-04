f69b6640683a20e7843714d6b9343411607f605d
Niklas Köhn <n.koehn@esri.de>
2017-07-04 15:06:13 +0200

    Initial Commit

---
+++ nls/de/strings.js
@@ -0,0 +1,8 @@
+﻿define({
+    root:({
+        _widgetLabel: "Kaufland Arbeitsablauf NLS",
+        bufferLabel: "Puffer um selektierten Ort erstellen",
+        bufferRadius: "Puffer-Radius:",
+        bufferGenerateButton: "Generieren"
+    }) 
+});
