fae568001ca71253dbef5ce0f25c0459184df0ba
Niklas Köhn <n.koehn@esri.de>
2018-09-18 17:35:38 +0200

    jimu.d.ts: No official typescript definitions for the Jimu framework available. The most commonly used classes during widget development are covered here.

---
+++ jimu.d.ts/Manifest.d.ts
@@ -0,0 +1,11 @@
+export interface IManifest{
+    name: string;
+    platform: string;
+    version: string;
+    wabVersion: string;
+    author: string;
+    description: string;
+    copyright: string;
+    license: string;
+    properties: {};
+}
